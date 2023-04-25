f348a81e21127b9f4bbfa32b69537224d475e3f5
Peter Houghton <pete@investigatingsoftware.co.uk>
2023-04-25 13:53:52 +0000

    Added summary comment#

--- clean_data.py
+++ clean_data.py
@@ -7,6 +7,10 @@ from azure.core.credentials import AzureKeyCredential
 from decouple import config
 import matplotlib.dates as mdates
 
+# This code reads an Excel file containing a time series of monthly death data,
+# preprocesses the data, and identifies anomalies using the Azure
+# Anomaly Detector API. After detecting anomalies, it plots the time series,
+# highlights the anomalies, and shows upper and lower margin bands.
 
 # Read in excel file into pandas dataframe
 df = pd.read_excel('summary/summary_ons_deaths.xlsx',
@@ -24,7 +28,8 @@ print(df)
 df['Year'] = df['Year'].astype(int)
 
 # Iterate over month columns and change data type to integer
-month_columns = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+month_columns = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+                 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 for month in month_columns:
     df[month] = df[month].astype(int)
@@ -37,7 +42,8 @@ df = df.drop(columns=['Location'])
 print(df)
 
 # Melt the DataFrame
-df_melted = pd.melt(df, id_vars=['Year'], var_name='Month', value_name='Deaths')
+df_melted = pd.melt(df, id_vars=['Year'],
+                    var_name='Month', value_name='Deaths')
 
 # Change month names to numbers
 month_mapping = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5,
@@ -52,18 +58,17 @@ df_melted['Date'] = pd.to_datetime(df_melted[['Year', 'Month']].assign(day=1))
 df_sorted = df_melted.sort_values(by='Date').reset_index(drop=True)
 
 # Drop unnecessary columns and keep only the Deaths column
-df_final = df_sorted[['Date', 'Deaths']]
+df = df_sorted[['Date', 'Deaths']]
 
+
+# Create a list of dictionaries for the series, thats what azure expects
 series = []
-# iterate over the rows in the data frame to create a list of dictionaries
-# each dictionary contains the date and the deaths
-# this is the format required by the anomaly detector
-for index, row in df_final.iterrows():
+for index, row in df.iterrows():
     series.append({'timestamp': row['Date'], 'value': row['Deaths']})
 
 print(series)
 
-# create a request data object for the azxure anomaly detector
+# Create a request data object for the azxure anomaly detector
 request_data = {
     'series': series,
     'granularity': 'monthly',
@@ -77,44 +82,34 @@ client = AnomalyDetectorClient(
     credential=AzureKeyCredential(config('API_KEY')),
     endpoint=config('API_ENDPOINT'))
 
-# call the anomaly detector
+# Call the anomaly detector
 anomaly_response = client.detect_univariate_entire_series(request_data)
 
-print(anomaly_response.is_anomaly)
-# add the is_anomaly column to the dataframe
-df_final['is_anomaly'] = anomaly_response.is_anomaly
-
-print("upper values")
-print(anomaly_response.upper_margins)
-
-print("low values")
-print(anomaly_response.lower_margins)
+df['is_anomaly'] = anomaly_response.is_anomaly
 
 # Add the margins list to the DataFrame
-df_final['upper_margin_deltas'] = anomaly_response.upper_margins
-df_final['lower_margin_deltas'] = anomaly_response.lower_margins
+df['upper_margin_delta'] = anomaly_response.upper_margins
+df['lower_margin_delta'] = anomaly_response.lower_margins
+df['upper_margin'] = df['Deaths'] + df['upper_margin_delta']
+df['lower_margin'] = df['Deaths'] - df['lower_margin_delta']
 
+# Print dataframe head as debug
+print(df.head())
 
-df_final['upper_margin'] = df_final['Deaths'] + df_final['upper_margin_deltas']
-df_final['lower_margin'] = df_final['Deaths'] - df_final['lower_margin_deltas']
-
-# print fill dataframe
-print(df_final)
-
-mask = df_final['is_anomaly']
+mask = df['is_anomaly']
 
 fig, ax = plt.subplots(figsize=(15, 6))  # Adjust the figure size
 
 # Plot the time series
-ax.plot(df_final['Date'], df_final['Deaths'], label='Deaths')
+ax.plot(df['Date'], df['Deaths'], label='Deaths')
 
 # Plot the red markers where 'is_anomaly' is True
-ax.scatter(df_final.loc[mask, 'Date'], df_final.loc[mask,
+ax.scatter(df.loc[mask, 'Date'], df.loc[mask,
            'Deaths'], color='red', marker='o', label='Anomaly')
 
 # Add the shaded area between the upper and lower margins
-ax.fill_between(df_final['Date'], df_final['lower_margin'],
-                df_final['upper_margin'], color='gray',
+ax.fill_between(df['Date'], df['lower_margin'],
+                df['upper_margin'], color='gray',
                 alpha=0.3, label='Margin')
 
 # Configure the x-axis to show year markers
@@ -127,6 +122,5 @@ ax.set_ylabel('Deaths')
 ax.set_title('Deaths Time Series with Anomalies and Margins')
 ax.legend()
 
-# # Save the plot with a higher resolution (e.g., 300 dpi)
+# Save the plot with a higher resolution (e.g., 300 dpi)
 plt.savefig('summary/summary_ons_deaths.png', dpi=150, bbox_inches='tight')
-
